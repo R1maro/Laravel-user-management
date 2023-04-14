756157015085987d05d747d6141e5c8ee7030ab4
R1maro <amir.mmdi1221@gmail.com>
2023-04-14 22:29:52 +0430

    one User seeder test

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Training;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+         \App\Models\User::factory()->create([
+             'name' => 'R1maro',
+             'email' => 'admin@example.com',
+         ]);
+
+    }
+}
