9ccf819232410f2365a0b655f35998296e2e9b88
R1maro <amir.mmdi1221@gmail.com>
2023-04-28 01:05:13 +0430

    phone_number problem fixed for create 100 fake user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('mobile',15)->unique();
+            $table->string('mobile',20)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
