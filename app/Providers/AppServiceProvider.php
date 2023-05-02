50facbc7770bc8b0f2175f67f12217069bf91a7c
R1maro <amir.mmdi1221@gmail.com>
2023-05-03 01:55:47 +0430

    ide_helper installed successfully

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
