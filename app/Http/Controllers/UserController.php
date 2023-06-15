c08a4e38539a57103e1300662b042905b261fe4f
R1maro <amir.mmdi1221@gmail.com>
2023-06-16 00:47:33 +0330

    also show message for user update and user delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -72,7 +72,7 @@ class UserController extends Controller
     {
         //
         $this->CreateOrUpdate($user,$request);
-        return redirect()->route('user.index');
+        return redirect()->route('user.index')->with(['message' => 'User updated']);
     }
 
     /**
@@ -82,7 +82,7 @@ class UserController extends Controller
     {
         //
         $user->delete();
-        return redirect()->route('user.index');
+        return redirect()->route('user.index')->with(['message' => 'User deleted']);
 
     }
 }
