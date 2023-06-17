4261ccf10bfa4025ebd70bc5781ad344bed06c45
R1maro <amir.mmdi1221@gmail.com>
2023-06-17 23:49:19 +0330

    create group route for users route

--- routes/web.php
+++ routes/web.php
@@ -50,26 +50,39 @@ Route::get('/about',function (){
 });
 
 
-Auth::routes(['register'=>false]);
+Auth::routes(['register'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/users',[\App\Http\Controllers\UserController::class,'index'])->name('user.index');
+//
+//
+//Route::get('/users',[\App\Http\Controllers\UserController::class,'index'])->name('user.index');
+//
+//Route::get('/users/create',[\App\Http\Controllers\UserController::class,'create'])
+//    ->name('user.create');
+//
+//Route::get('/users/edit/{user}',[\App\Http\Controllers\UserController::class,'edit'])
+//    ->name('user.edit');
+//
+//Route::post('/users/store',[\App\Http\Controllers\UserController::class,'store'])
+//    ->name('user.store');
+//
+//Route::post('/users/update/{user}',[\App\Http\Controllers\UserController::class,'update'])
+//    ->name('user.update');
+//
+//Route::get('/users/delete/{user}', [\App\Http\Controllers\UserController::class, 'destroy'])
+//    ->name('user.delete');
 
-Route::get('/users/create',[\App\Http\Controllers\UserController::class,'create'])
-    ->name('user.create');
 
-Route::get('/users/edit/{user}',[\App\Http\Controllers\UserController::class,'edit'])
-    ->name('user.edit');
-
-Route::post('/users/store',[\App\Http\Controllers\UserController::class,'store'])
-    ->name('user.store');
-
-Route::post('/users/update/{user}',[\App\Http\Controllers\UserController::class,'update'])
-    ->name('user.update');
-
-Route::get('/users/delete/{user}', [\App\Http\Controllers\UserController::class, 'destroy'])
-    ->name('user.delete');
+Route::prefix('users')->name('user.')->group(
+    function () {
+        Route::get('/', [\App\Http\Controllers\UserController::class,'index'])->name('index');
+        Route::get('/create', [\App\Http\Controllers\UserController::class,'create'])->name('create');
+        Route::get('/edit/{user}', [\App\Http\Controllers\UserController::class,'edit'])->name('edit');
+        Route::post('/store', [\App\Http\Controllers\UserController::class,'store'])->name('store');
+        Route::post('/update/{user}', [\App\Http\Controllers\UserController::class,'update'])->name('update');
+        Route::get('/delete/{user}', [\App\Http\Controllers\UserController::class,'destroy'])->name('delete');
+    });
 
 
