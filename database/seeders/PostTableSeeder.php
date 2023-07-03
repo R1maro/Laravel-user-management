5afbf310599aba610653c96f4af32815d5e59711
R1maro <amir.mmdi1221@gmail.com>
2023-07-03 19:20:38 +0330

    create Post factory for make fake posts call factory in seeder for fake data count

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+
+
+        \App\Models\Post::factory(100)->create();
+
+
+//        \App\Models\Post::factory(100)->create();
+
+//        for ($i = 0; $i < 20; $i++ ){
+//            $p = new Post();
+//            $p->title = 'my title'.$i;
+//            $p->slug = 'my title'.$i;
+//            $p->body = 'body';
+//            $p->save();
+//
+//        }
+    }
+}
