da45a2db2c4b25753bc0ce1972abb821ae95660f
R1maro <amir.mmdi1221@gmail.com>
2023-07-03 19:19:13 +0330

    validate fake users seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PostTableSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,5 +13,6 @@ class PostTableSeeder extends Seeder
     public function run(): void
     {
         //
+        \App\Models\User::factory(100)->create();
     }
 }
