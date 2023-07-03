91a45984b3f496f806c2f68a4348f8465cf49fc2
R1maro <amir.mmdi1221@gmail.com>
2023-07-03 19:21:11 +0330

    add soft delete trait to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 }
