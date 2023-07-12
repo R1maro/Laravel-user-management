093847c0eb2609aa32b8d56b59371a343639e74d
amir <amir.mmdi1221@gmail.com>
2023-07-13 02:32:42 +0330

    create a btn with route(PostController/Create) for create new post

--- resources/views/post/postList.blade.php
+++ resources/views/post/postList.blade.php
@@ -6,6 +6,13 @@
 
     @include('component.err')
 
+
+    <div class="mb-3">
+        <a href="{{route('post.create',)}}" class="btn btn-outline-warning d-block w-50 m-auto p-2 fs-5">
+            Create new post
+        </a>
+    </div>
+
     <div class="row">
         @foreach($posts as $post)
             <div class="col-md-4">
