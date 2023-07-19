c7a51bb39fedfe7f7ae836171b2c31fe26a6b9a8
R1maro <amir.mmdi1221@gmail.com>
2023-07-19 16:34:12 +0330

    get img file whit this command in line 21 and save this in our public folder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
         $post->body = $request->body;
         $post->slug = str_replace('', '-', $post->title);
         if ($request->hasFile('img')) {
-            dd($request->File('img'));
+            $request->File('img')->storePubliclyAs("/public/posts".$post->slug.'.jpg');
         }
         $post->save();
         return $post;
