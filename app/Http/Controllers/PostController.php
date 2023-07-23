6ce22b9f37dc6bb14f497998e0b0fdfb5a473f3e
R1maro <amir.mmdi1221@gmail.com>
2023-07-23 19:57:32 +0330

    create new blade file for show single post show return that view in PostController.php show function create route for show post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -65,6 +65,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         //
+        return view('post.post',compact('post'));
     }
 
     /**
---
+++ resources/views/post/post.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+    <h1>
+        Post {{$post->title}}
+    </h1>
+
+    @include('component.err')
+
+    <div class="container">
+
+
+    </div>
+
+@endsection
