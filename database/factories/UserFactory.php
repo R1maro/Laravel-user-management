7a096b922539502e6b5743776cd89072b6acfdc9
R1maro <amir.mmdi1221@gmail.com>
2023-07-27 00:12:01 +0330

    better condition for credit value create(more users taking credit)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'mobile' => fake()->unique()->phoneNumber(),
-            'credit' => rand(0, 10) == 0 ? rand(1000, 10000) * 1000 : 0,
+            'credit' => rand(0, 1) == 0 ? rand(1000, 10000) * 1000 : 0,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
