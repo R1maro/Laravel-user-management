4af8fd477679eac10ea6615d40265f302d7c1cda
R1maro <amir.mmdi1221@gmail.com>
2023-07-26 23:55:50 +0330

    create random credit for users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'mobile' => fake()->unique()->phoneNumber(),
+            'credit' => rand(0, 10) == 0 ? rand(1000, 10000) * 1000 : 0,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -32,7 +33,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
