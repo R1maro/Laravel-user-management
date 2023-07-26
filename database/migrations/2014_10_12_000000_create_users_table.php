e9b27e2e21458cc7c477c81aa800f318ca9f7922
R1maro <amir.mmdi1221@gmail.com>
2023-07-26 23:50:53 +0330

    create credit column for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('mobile',20)->unique()->nullable();
+            $table->unsignedBigInteger('credit')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->softDeletes();
