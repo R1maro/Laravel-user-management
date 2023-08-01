29ec3ffa9d661a761a0253ebe5108210ace4eac3
R1maro <amir.mmdi1221@gmail.com>
2023-08-01 16:52:47 +0330

    create vue component for datepicker

---
+++ resources/js/components/DatePicker.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "DatePicker"
+})
+</script>
+
+<template>
+    <div>
+        DatePicker
+    </div>
+</template>
+
+<style scoped>
+
+</style>
